21417b3fc48e69e6b0e31cf1cc994aa0888c1bf3
OGnTn <peschken23@gmail.com>
2022-12-05 15:46:16 +0100

    Cleaned up parser script and added mongo query notebook

--- mongo_parser.py
+++ mongo_parser.py
@@ -8,47 +8,18 @@ import numpy as np
 import pymongo
 import re
 
-XML_FILE = "miauw.xml"
-
 mongoclient = pymongo.MongoClient("mongodb://localhost:27017/")
 testdb = mongoclient["testdb"]
 testarticlescol = testdb["articles"]
 testproceedingcol = testdb["proceedings"]
 testinproceedingcol = testdb["inproceedings"]
 
-
-def get_all(element, tag):
-    elements = np.array(list(map(lambda x: x.text, element.findall(tag))))
-    if len(elements) > 1:
-        values = " --- ".join(elements)
-        return values
-    elif len(elements) == 1:
-        return elements[0]
-    else:
-        return np.nan
-
-
-# attribute to parse article = ['key','title','journal','volume','number','year']
-# attribute to parse proceedings = ['key','editor','title','publisher','year','booktitle','volume']
-# attribute to parse inproceedings = ['key','author','title','pages','year','booktitle']
 article_attributes = ['key', 'author', 'title',
                       'journal', 'volume', 'number', 'year']
 proceeding_attributes = ['key', 'editor', 'title', 'booktitle',
                          'publisher', 'volume', 'year']
 inproceeding_attributes = ['key', 'author',
                            'title', 'pages', 'year', 'booktitle']
-
-miauw = [{"test": "woef"}]
-
-
-def map_or_value(map):
-    if(len(list(map)) > 1):
-        # print(list(map))
-        return list(map)
-    elif (len(list(map)) == 1):
-        return list(map)[0]
-    else:
-        return np.nan
 
 
 def mymap(fun, list):
@@ -64,7 +35,6 @@ def mymap(fun, list):
 
 
 def parse(xml_file):
-    # make sure to include key in attributes
     article_data = dict(
         zip(article_attributes, [[] for attrib in range(len(article_attributes))]))
     proceeding_data = dict(
@@ -77,16 +47,13 @@ def parse(xml_file):
     proceedings = []
     for _, element in etree.iterparse(source=xml_file, dtd_validation=True, load_dtd=True):
         if element.tag == "article":
+
             current_article = {}
             current_article["key"] = element.attrib.get('key')
             for attribute in list(filter(lambda x: x != 'key', article_attributes)):
                 current_article[attribute] = mymap(
                     lambda x: x.text, element.findall(attribute))
             articles.append(current_article)
-            # adding them to the database(this case dict in order to be transformed into pandas dataframe and then csv file)
-            # article_data["key"].append(element.attrib.get('key'))
-            # for attribute in list(filter(lambda x: x != 'key', article_attributes)):
-            #    article_data[attribute].append(get_all(element, attribute))
             element.clear()
         elif element.tag == "proceedings":
 
@@ -96,11 +63,6 @@ def parse(xml_file):
                 current_proceeding[attribute] = mymap(
                     lambda x: x.text, element.findall(attribute))
             proceedings.append(current_proceeding)
-
-            # adding them to the database(this case dict in order to be transformed into pandas dataframe and then csv file)
-            # proceeding_data["key"].append(element.attrib.get('key'))
-            # for attribute in list(filter(lambda x: x != 'key', proceeding_attributes)):
-            #    proceeding_data[attribute].append(get_all(element, attribute))
             element.clear()
         elif element.tag == "inproceedings":
 
@@ -110,20 +72,8 @@ def parse(xml_file):
                 current_inproceeding[attribute] = mymap(
                     lambda x: x.text, element.findall(attribute))
             inproceedings.append(current_inproceeding)
-
-            # adding them to the database(this case dict in order to be transformed into pandas dataframe and then csv file)
-            # inproceeding_data["key"].append(element.attrib.get('key'))
-            # for attribute in list(filter(lambda x: x != 'key', inproceeding_attributes)):
-            #    inproceeding_data[attribute].append(
-            #        get_all(element, attribute))
             element.clear()
     print("entries parsed.")
-    #article_df = pd.DataFrame(article_data)
-    # article_df.to_csv("article.csv")
-    #proceeding_df = pd.DataFrame(proceeding_data)
-    # proceeding_df.to_csv("proceeding.csv")
-    #inproceeding_df = pd.DataFrame(inproceeding_data)
-    # inproceeding_df.to_csv("inproceeding.csv")
     print("inserting articles.")
     testarticlescol.insert_many(articles)
     print("articles inserted.")
@@ -133,7 +83,6 @@ def parse(xml_file):
     print("inserting proceedings.")
     testproceedingcol.insert_many(proceedings)
     print("inproceedings inserted.")
-
     print("file parsed")
 
 
